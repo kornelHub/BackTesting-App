026a0461b0da083b45ce9f9891a20d23883aaae7
kornelHub <67650984+kornelHub@users.noreply.github.com>
2021-01-24 18:35:22 +0100

    added data_path autofill QLineEdit in top toolbar menu

--- main_page.py
+++ main_page.py
@@ -1,6 +1,7 @@
 import os
 from PySide2 import QtGui, QtWidgets, QtCore
 from PySide2.QtUiTools import loadUiType
+from PySide2.QtWidgets import QFileDialog
 from functools import partial
 from engine import simulation
 
@@ -23,18 +24,21 @@ class MainWidget(Base, Form):
         for i, button in enumerate(buttons):
             button.clicked.connect(partial(self.widget_pages.setCurrentIndex, i))
 
-        self.btn_toggle.clicked.connect(self.toggleMenu)
+        # Top toolbar menu
+        self.btn_toggle.clicked.connect(lambda: self.toggleMenu())
+        self.data_button.clicked.connect(lambda: self.add_path_to_data())
         self.next_btn.clicked.connect(lambda: self.display_next_page())
 
-########################################################################################################################
-### Strategy Page ###
-########################################################################################################################
+        # Strategy page
         self.strategy_page.p2_add_buy_rule.clicked.connect(lambda: self.strategy_page.display_add_strategy_rule_page_buy_context(main_widget_object.strategy_page))
         self.strategy_page.p2_edit_buy_rule.clicked.connect(lambda: self.strategy_page.display_add_strategy_rule_page_modify_buy_context())
 
         self.strategy_page.p2_add_sell_rule.clicked.connect(lambda: self.strategy_page.display_add_strategy_rule_page_sell_context(main_widget_object.strategy_page))
         self.strategy_page.p2_edit_sell_rule.clicked.connect(lambda: self.strategy_page.display_add_strategy_rule_page_modify_sell_context())
-########################################################################################################################
+
+        # Fetch data page
+        self.fetch_data_page.p1_saveDataToFile_button.clicked.connect(lambda: self.fetch_data_page.fetch_data_btn_clicked(main_widget_object, current_dir))
+
     def toggleMenu(self):
         width = self.frame_left_menu_container.width()
         maxExtend = 250
@@ -50,6 +54,10 @@ class MainWidget(Base, Form):
         self.animation.setEndValue(widthExtended)
         self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
         self.animation.start()
+
+    def add_path_to_data(self):
+        path_to_file = QFileDialog.getOpenFileName(self, 'Load CSV file with OHLCV data', current_dir + '\data', 'Text Files (*.csv)')
+        self.data_path.setText(path_to_file[0])
 
     def display_next_page(self):
         ### start simulation
--- pages/fetch_data_page.py
+++ pages/fetch_data_page.py
@@ -14,9 +14,8 @@ class Fetch_Data_Widget(Base, Form):
         super(self.__class__, self).__init__(parent)
         self.setupUi(self)
         self.p1_ohlcvPlot_qWebEngineView.hide()
-        self.p1_saveDataToFile_button.clicked.connect(self.fetch_data_btn_clicked)
 
-    def fetch_data_btn_clicked(self):
+    def fetch_data_btn_clicked(self, main_widget_object, path_to_project):
         start_date = self.p1_startDate_textField_day.text() + ' ' + self.p1_startDate_comboBox_month.currentText() \
                      + ' ' + self.p1_startDate_textField_year.text() + ' ' + self.p1_startDate_textField_hour.text() \
                      + ':' + self.p1_startDate_textField_minute.text() + ':' + self.p1_startDate_textField_second.text()
@@ -26,16 +25,11 @@ class Fetch_Data_Widget(Base, Form):
 
         currency_symbol = self.p1_cryptoSymbol_textField.text()
         interval = self.p1_interval_dropdown.currentText()
-        print('currency_symbol: ', currency_symbol)
-        print('interval: ', interval)
-        print('start_date: ', start_date)
-        print(date_to_milliseconds(start_date))
-        print('end_date: ', end_date)
-        print(date_to_milliseconds(end_date))
-        path_to_file = QFileDialog.getSaveFileName(self, 'Save OHLCV data tpo CSV files', 'D:\!python_projects\praca_inz_qt\data', 'Text Files (*.csv)')
+        path_to_file = QFileDialog.getSaveFileName(self, 'Save OHLCV data to CSV file', path_to_project + '\data', 'Text Files (*.csv)')
         fetch_data.create_csv_with_ohlcv_data(start_time=int(date_to_milliseconds(start_date)), end_time=int(date_to_milliseconds(end_date)), currency_pair_symbol=currency_symbol, interval=interval, path_to_file=path_to_file[0])
         self.p1_ohlcvPlot_qWebEngineView.show()
         self.p1_ohlcvPlot_qWebEngineView.setHtml(plot_data.plot_ohlcv_data(path_to_file[0]))
+        main_widget_object.data_path.setText(path_to_file[0])
 
 
 if __name__ == '__fetch_data_page__':
