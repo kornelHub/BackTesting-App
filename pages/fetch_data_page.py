5cb34e18374241931a444ad7455309b7546346a1
kornelHub <67650984+kornelHub@users.noreply.github.com>
2021-02-28 11:03:56 +0100

    added validator(acceptable values) to input fields

--- pages/fetch_data_page.py
+++ pages/fetch_data_page.py
@@ -1,5 +1,7 @@
 import os
 from PySide2 import QtGui
+from PySide2.QtCore import QRegExp
+from PySide2.QtGui import QRegExpValidator
 from PySide2.QtUiTools import loadUiType
 from PySide2.QtWidgets import QFileDialog
 from binance.helpers import date_to_milliseconds
@@ -14,6 +16,29 @@ class Fetch_Data_Widget(Base, Form):
         super(self.__class__, self).__init__(parent)
         self.setupUi(self)
         self.p1_ohlcvPlot_qWebEngineView.hide()
+
+        # allow number in range 1-31 included, in format 5 or 05
+        day_regular_expression = QRegExp("[1-9]|0[0-9]|[12][0-9]|3[01]")
+        self.p1_startDate_textField_day.setValidator(QRegExpValidator(day_regular_expression, self))
+        self.p1_endDate_textField_day.setValidator(QRegExpValidator(day_regular_expression, self))
+
+        # allow number in range 0-23 included, in format 5 or 05
+        hour_regular_expression = QRegExp("[0-9]|0[0-9]|1[0-9]|2[0-3]")
+        self.p1_startDate_textField_hour.setValidator(QRegExpValidator(hour_regular_expression, self))
+        self.p1_endDate_textField_hour.setValidator(QRegExpValidator(hour_regular_expression, self))
+
+        # allow number in range 0-59  included, in format 5 or 05
+        minute_or_second_regular_expression = QRegExp("[0-9]|0[0-9]|[1-5][0-9]")
+        self.p1_startDate_textField_minute.setValidator(QRegExpValidator(minute_or_second_regular_expression, self))
+        self.p1_startDate_textField_second.setValidator(QRegExpValidator(minute_or_second_regular_expression, self))
+        self.p1_endDate_textField_minute.setValidator(QRegExpValidator(minute_or_second_regular_expression, self))
+        self.p1_endDate_textField_second.setValidator(QRegExpValidator(minute_or_second_regular_expression, self))
+
+        # allow number in range 2018-2029 included
+        year_regular_expression = QRegExp("[2][0](1[8-9]|2[0-9])")
+        self.p1_startDate_textField_year.setValidator(QRegExpValidator(year_regular_expression, self))
+        self.p1_endDate_textField_year.setValidator(QRegExpValidator(year_regular_expression, self))
+
 
     def fetch_data_btn_clicked(self, main_widget_object, path_to_project):
         start_date = self.p1_startDate_textField_day.text() + ' ' + self.p1_startDate_comboBox_month.currentText() \
