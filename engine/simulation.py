0a44372c386886a0038fc26aa1b9f23800e060c5
kornelHub <67650984+kornelHub@users.noreply.github.com>
2021-04-27 19:33:29 +0200

    adjust html_report display

--- engine/simulation.py
+++ engine/simulation.py
@@ -356,5 +356,5 @@ def init_simulation(main_window_object):
     # print(json.dumps(trades_dict, indent=4))
 
     # pass and display data in summary_page
-    main_window_object.summary_page.format_and_display_text(trades_dict, pip_position)
     main_window_object.summary_page.display_buy_and_sell_rules(buy_rules, sell_rules, sell_simulation_settings)
+    main_window_object.summary_page.format_and_display_text(trades_dict, pip_position)
--- utilities/generate_report.py
+++ utilities/generate_report.py
@@ -1,3 +1,6 @@
+from PySide2 import QtCore
+from engine.simulation import check_if_parent_exist
+
 report_template_part_1 = """<!DOCTYPE html>
 <html>
 <style>
@@ -20,7 +23,6 @@ report_template_part_1 = """<!DOCTYPE html>
 }
 
 .right_container {
-  display: flex;
   width:auto;
   overflow:hidden;
 }
@@ -36,12 +38,11 @@ report_template_part_1 = """<!DOCTYPE html>
 }
 
 .rule{
-  height:300px;
-  width: 100%;
   background-color: rgb(55,55,55);
   border-radius:10px 10px 10px 10px;
   display: inline-block;
   margin-top: 15px;
+  vertical-align: top;
 }
 
 .rule:first-of-type{
@@ -61,36 +62,89 @@ report_template_part_1 = """<!DOCTYPE html>
 }
 
 .text_inside{
+  display: inline-block;
   font: 10pt 'MS Shell Dlg 2';
   color: rgb(255,255,255);
   white-space: pre-line;
   margin-right: 15px;
   margin-left: 15px;
-}"""
+}
 
-def callback_function(html):
-    return html
+    </style>
+    <body style='background-color:rgb(45,45,45);' onload='adjust_rule_size();'>
+    <script>
+    function adjust_rule_size() {
+      var buy_rule_width = document.getElementById('buy_rule').clientWidth
+      var sell_rule_width = document.getElementById('sell_rule').clientWidth
+      if (buy_rule_width > sell_rule_width) {
+        document.getElementById('sell_rule').style.width=buy_rule_width.toString().concat('px')
+      } else {
+        document.getElementById('buy_rule').style.width=sell_rule_width.toString().concat('px')
+      }
+
+      var buy_rule_id_width = document.getElementById('buy_rule_id').clientWidth
+      var sell_rule_id_width = document.getElementById('sell_rule_id').clientWidth
+      if (buy_rule_id_width > sell_rule_id_width) {
+        document.getElementById('sell_rule_id').style.width=buy_rule_id_width.toString().concat('px')
+      } else {
+        document.getElementById('buy_rule_id').style.width=sell_rule_id_width.toString().concat('px')
+      }
+      
+      var buy_rule_id_height = document.getElementById('buy_rule_id').clientHeight
+      var sell_rule_id_height = document.getElementById('sell_rule_id').clientHeight
+      if (buy_rule_id_height > sell_rule_id_height) {
+        document.getElementById('sell_rule_id').style.height=buy_rule_id_height.toString().concat('px')
+      } else {
+        document.getElementById('buy_rule_id').style.height=sell_rule_id_height.toString().concat('px')
+      }
+    }
+</script>
+"""
 
 def generate_html_report_to_file(summary_page, html_of_graphs):
     html_of_graphs = "<div style='height:1200px;'>" + html_of_graphs[17:-14]
 
+    buy_rules = summary_page.buy_rules_treeWidget.findItems('', QtCore.Qt.MatchContains | QtCore.Qt.MatchRecursive, 0)
+    # buy_rules_id = summary_page.buy_rules_treeWidget.findItems('', QtCore.Qt.MatchContains | QtCore.Qt.MatchRecursive, 1)
+    buy_rules_text = ''
+    buy_rules_id_text = ''
+
+    sell_rules = summary_page.sell_rules_treeWidget.findItems('', QtCore.Qt.MatchContains | QtCore.Qt.MatchRecursive, 0)
+    # sell_rules_id = summary_page.sell_rules_treeWidget.findItems('', QtCore.Qt.MatchContains | QtCore.Qt.MatchRecursive, 1)
+    sell_rules_text = ''
+    sell_rules_id_text = ''
+
+
+    for x in buy_rules:
+        buy_rules_text += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'* check_if_parent_exist(x, 0) + x.text(0) + '\n'
+        buy_rules_id_text += '| '+x.text(1) + '\n'
+
+
+    for x in sell_rules:
+        sell_rules_text += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'* check_if_parent_exist(x, 0) + x.text(0) + '\n'
+        sell_rules_id_text += '| '+x.text(1) + '\n'
+
+    transaction_text = summary_page.transactions_textBrowser.toPlainText().replace('      ', '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;')
+    # transaction_text = transaction_text.replace('-'*76, '-'*20)
+    print(transaction_text)
+
     report_template_part_2 = f"""
-    </style>
-    <body style='background-color:rgb(45,45,45);'>
     <div class='container'>
         <div class='left_container'>
            <p class='label'>Transactions</p>
-           <p class='text_inside'>{summary_page.transactions_textBrowser.toPlainText()}</p>
+           <p class='text_inside'>{transaction_text}</p>
         </div>
 
         <div class='right_container'>
           <div class='rule'>
             <p class='label'>Buy rule</p>
-            <p class='text_inside'>{2137}</p>
+            <p class='text_inside' id='buy_rule'>{buy_rules_text}</p>
+            <p class='text_inside' id='buy_rule_id'>{buy_rules_id_text}</p>
           </div>
           <div class='rule'>
             <p class='label'>Sell rule</p>
-            <p class='text_inside'>{42069}</p>
+            <p class='text_inside' id='sell_rule'>{sell_rules_text}</p>
+            <p class='text_inside' id='sell_rule_id'>{sell_rules_id_text}</p>
           </div>
         </div>
 
